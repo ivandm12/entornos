f3313a14124338745fdcf9c587988f87bf46b72f
Ivan <Ivan@DESKTOP-6HPVFGV>
2023-06-04 22:41:36 +0200

    hola mundo2

--- src/proyectoentornos/ProyectoEntornos.java
+++ src/proyectoentornos/ProyectoEntornos.java
@@ -14,6 +14,6 @@ public class ProyectoEntornos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        System.out.println("Hola Mundo!!!");
     }
 }
